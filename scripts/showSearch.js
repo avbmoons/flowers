983e8253de5103bd8d7279f91ea3a7e0880692b7
Elena-admin <avb.moons@gmail.com>
2024-02-12 22:36:19 +0300

    init commit

---
+++ scripts/showSearch.js
@@ -0,0 +1,13 @@
+const searchInput = document.getElementsByClassName("search-input-box")[0];
+//const toSearch = document.getElementsByClassName("to-search")[0];
+
+console.log(searchInput);
+//console.log(toSearch);
+
+function showSearch() {
+  searchInput.style.display = "flex";
+}
+function closeSearch() {
+  searchInput.style.display = "none";
+}
+//toSearch.addEventListener("click", showSearch());
